798f994f92659b8805c2decf97740391100bb830
kumar <sivakumarlella26@email.com>
2024-03-12 09:15:54 +0530

    added terraform

---
+++ terraform/eks-cluster.tf
@@ -0,0 +1,17 @@
+# EKS
+
+module "eks" {
+  source                  = "./module/eks"
+  aws_public_subnet       = module.vpc.aws_public_subnet
+  vpc_id                  = module.vpc.vpc_id
+  cluster_name            = "module-eks-${random_string.suffix.result}"
+  endpoint_public_access  = true
+  endpoint_private_access = false
+  public_access_cidrs     = ["0.0.0.0/0"]
+  node_group_name         = "cloudquicklabs"
+  scaling_desired_size    = 1
+  scaling_max_size        = 1
+  scaling_min_size        = 1
+  instance_types          = ["t3.small"]
+  key_pair                = "singapore-new"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,2 @@
+variable "vpc_cidr_block" {}
+variable "public_subnet_cidr_blocks" {}
